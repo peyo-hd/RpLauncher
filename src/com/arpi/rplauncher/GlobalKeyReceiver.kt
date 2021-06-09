ac31ff7777daf3ca722073534f970092610273fc
Peter Yoon <peyo.hd@gmail.com>
2021-06-09 13:13:38 +0900

    GlobalKeyReceiver: invoke vosk on F7 key

--- src/com/arpi/rplauncher/GlobalKeyReceiver.kt
+++ src/com/arpi/rplauncher/GlobalKeyReceiver.kt
@@ -2,6 +2,7 @@ package com.arpi.rplauncher
 
 import android.app.ActivityOptions
 import android.content.BroadcastReceiver
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.view.KeyEvent
@@ -16,7 +17,7 @@ class GlobalKeyReceiver : BroadcastReceiver() {
                 if (event.action == KeyEvent.ACTION_DOWN) {
                     when (event.keyCode) {
                         KeyEvent.KEYCODE_F1 -> startHomeOnPrimaryDisplay(context)
-                        KeyEvent.KEYCODE_F7 -> startSettingsOnPrimaryDisplay(context)
+                        KeyEvent.KEYCODE_F7 -> startVoskDemoOnPrimaryDisplay(context)
                     }
                 }
             }
@@ -32,11 +33,10 @@ class GlobalKeyReceiver : BroadcastReceiver() {
         context.startActivity(intent, bundle)
     }
 
-    private val ACTION_SETTINGS = "android.settings.SETTINGS"
-
-    private fun startSettingsOnPrimaryDisplay(context: Context) {
-        val intent = Intent(ACTION_SETTINGS)
+    private fun startVoskDemoOnPrimaryDisplay(context: Context) {
+        val intent = Intent(Intent.ACTION_MAIN)
             .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            .setComponent(ComponentName("org.vosk.demo","org.vosk.demo.MainActivity"))
         val bundle = ActivityOptions.makeBasic()
             .setLaunchDisplayId(0).toBundle()
         context.startActivity(intent, bundle)
